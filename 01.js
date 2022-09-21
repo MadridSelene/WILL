775fea22f7b7b5551e6accefa8039f8efedeaf09
MadridSelene <seleneailenmadrid@gmail.com>
2022-09-21 15:39:24 -0300

    01-Resuelto

--- 01.js
+++ 01.js
@@ -11,7 +11,13 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
+  var soloNros = [];
+  for (var i = 0; i < array.length; i++ ) {
+         if (Number.isInteger(array[i]) ) {
+            soloNros.push(array[i]);
+         }
+  }
+  return soloNros;
 }
 
 // No modifiques nada debajo de esta linea //
