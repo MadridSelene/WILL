3eea30ab11c36e569457c01edef4cab9931951fb
MadridSelene <seleneailenmadrid@gmail.com>
2022-09-22 16:36:16 -0300

    06-07-08-Resuelto

--- 06-07-08.js
+++ 06-07-08.js
@@ -9,25 +9,26 @@ function crearClasePersona() {
     constructor(nombre, edad, hobbies, amigos) {
       // El constructor de la clase Persona recibe nombre (string), edad (integer), hobbies (array de strings), amigos (array de objetos)
       // Inicializar las propiedades de la persona con los valores recibidos como argumento
-
-      // Tu código aca:
-
+      this.nombre = nombre;
+      this.edad = edad;
+      this.hobbies = hobbies;
+      this.amigos = amigos;
     }
 
     addFriend(nombre, edad) {
       // El método 'addFriend' recibe un string 'nombre' y un entero 'edad' y debe agregar un objeto:
       // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
       // No debe retornar nada.
-
       // Tu código aca:
-
+      var nuevoAmigo = {nombre: nombre, edad: edad};
+      this.amigos.push(nuevoAmigo);
     }
 
     addHobby(hobby) {
       // El método 'addHobby' recibe un string 'hobby' y debe agregarlo al arreglo de hobbies de la persona.
       // No debe retornar nada.
-
       // Tu código aca:
+      this.hobbies.push(hobby);
 
     }
     getFriends() {
@@ -36,8 +37,9 @@ function crearClasePersona() {
       // Ej:
       // Suponiendo que la persona tiene estos amigos: [{nombre: 'martin', edad: 31},{nombre: 'toni', edad: 33}]
       // persona.getFriends() debería devolver ['martin', 'toni']
-
       // Tu código aca:
+      return (this.amigos.map(function (elemento) { return elemento.nombre } ));
+      
 
     }
 
@@ -45,8 +47,8 @@ function crearClasePersona() {
       // El método 'getHobbies' debe retornar un arreglo con los hobbies de la persona
       // Ej:
       // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
-
       // Tu código aca:
+      return this.hobbies;
 
     }
 
@@ -66,7 +68,14 @@ function crearClasePersona() {
       // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
 
       // Tu código aca:
+      var total = 0;
+      for (var i = 0; i < this.amigos.length; i++ ) {
+        total = total + this.amigos[i].edad;
+      //total = total + this.amigos[i]["edad"];
+    }
+    return (total/ this.amigos.length);
 
+      
     }
   };
 
